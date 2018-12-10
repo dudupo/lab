804cc5484f022279a0d879e492c532a3b9b02f1f
dudu <dudupo51@gmail.com>
2018-12-10 23:08:47 +0200

    it ; EOF !
    /
    \n
    '
    s starting to look like something

--- SessionH.m
+++ SessionH.m
@@ -2,32 +2,33 @@ classdef SessionH < Session
     %UNTITLED3 Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties
-        XLABEL = "height relateive to the ground H[cm]";
-        YLABEL = "distance r[cm]";
-        YLABELVAR = "variance of clusters r[cm]"; 
-        XLABELVAR = "cluster distance H[cm]";
-    end
-    
     methods
         function obj = SessionH(G , material)
-            obj@Session(G , material);
+            obj@Session(G.H , G.R , material);
+            obj.XLABEL = "height relateive to the ground H[cm]";
+            obj.YLABEL = "distance r[cm]";
+            obj.YLABELVAR = "variance of clusters r[cm]"; 
+            obj.XLABELVAR = "cluster distance H[cm]";
         end
         
         function plot(obj)
             plot@Session(obj);
-            f = fit(obj.heights , obj.distances , "a*(x)^b +c" , 'StartPoint', [2, 0.5 , 0.01] , ...
-            'Upper', [140 , 0.6 , 0.01] , 'Lower' , [0 , 0.5 , 0.01 ] );
-            plot(f);
-            f 
+            
+            %f = fit(obj.heights , obj.distances , "a*(x)^b +c" , 'StartPoint', [2, 0.5 , 0.01] , ...
+            %'Upper', [140 , 0.6 , 0.01] , 'Lower' , [0 , 0.49 , 0.01 ] );
+            %plot(f);
+            %f 
             xlabel(obj.XLABEL);
             ylabel(obj.YLABEL);
 %             hold of
         end
-        function var(obj)
-            var@Session(obj);
-            xlabel(obj.XLABELVAR);
-            ylabel(obj.YLABELVAR);
+        
+        function ek = kinect(obj)
+            ek = kinect@Session(obj, obj.heights);
+        end
+        
+        function ek  = objkinect(obj)
+            ek = objkinect@Session(obj , obj.heights);
         end
     end
 end
--- Sessionsmallh.m
+++ Sessionsmallh.m
@@ -2,35 +2,41 @@
 classdef Sessionsmallh < Session
     %UNTITLED4 Summary of this class goes here
     %   Detailed explanation goes here
-    
-    properties
-        XLABEL = "height relateive to the trace h[cm]";
-        YLABEL = "Kinecket Energy K[J]";
-        YLABELVAR = "variance of clusters r[cm]"; 
-        XLABELVAR = "cluster distance h[cm]";
-    end
-    
+       
     methods
         function obj = Sessionsmallh(G , material)
-            obj@Session(G, material);
+            obj@Session(G.H , G.R , material);
+            obj.XLABEL = "height relateive to the trace h[cm]";
+            obj.YLABEL = "distance r[cm]";
+            obj.YLABELVAR = "variance of clusters r[cm]"; 
+            obj.XLABELVAR = "cluster distance h[cm]";
         end
         function plot(obj)
-            %plot@Session(obj);
-            gg = 10.981 * 10^2;
-            m = 0.1;
-            H = 10;
-            ek = obj.distances.^2 * gg^2 * m / (4*H);
-            errorbar(obj.heights, ek, obj.hbars , obj.hbars , obj.rbars.^2 , obj.rbars.^2 , '.');
-            legend("Ek");
-            f = fit(obj.heights ,ek , "b*(x)^a + d*x +c");
-            f
+            plot@Session(obj);
+            %gg = 10.981 * 10^2;
+            %m = 0.1;
+            %H = 10;
+            %ek = obj.distances.^2 * gg^2 * m / (4*H);
+            %errorbar(obj.heights, ek, obj.hbars , obj.hbars , obj.rbars.^2 , obj.rbars.^2 , '.');
+            %legend("Ek");
+            %f = fit(obj.heights ,ek , "b*(x)^a + d*x +c");
+            %f
 %             hold on;
-            plot(f);
+            %plot(f);
             xlabel(obj.XLABEL);
             ylabel(obj.YLABEL);
 %             hold of;
         end
-       
+        
+        function ek = kinect(obj)
+            Height = 10;
+            ek = kinect@Session(obj, Height);
+        end 
+        
+        function ek  = objkinect(obj)
+            ek = objkinect@Session(obj , obj.heights);
+            
+        end
         
         function var(obj)
             var@Session(obj);
